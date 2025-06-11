44eae86a7a4c495a858a4712ddb2122442803fa3
Maxime Hardy <maxime.bonneau.hardy@gmail.com>
2025-06-11 11:57:17 -0400

    Ajout d'un script d'entrainement(outside of jupyter)

---
+++ train.py
@@ -0,0 +1,52 @@
+import pathlib
+import numpy as np
+import pandas as pd
+import torch
+
+from thesession.dataset import TheSessionDataset
+from thesession.model import TheSessionModel
+from thesession.training import train_model
+
+# Datasets
+dataset = pd.read_csv("dataset.csv")
+
+test_subset = dataset.loc[dataset["dataset"] == "test", "tune"]
+val_subset = dataset.loc[dataset["dataset"] == "validation", "tune"]
+train_subset = dataset.loc[dataset["dataset"] == "train", "tune"]
+
+train_dataset = TheSessionDataset(
+    "audio_flac", subset=train_subset, sampling_rate=48000, fmt=".flac"
+)
+val_dataset = TheSessionDataset(
+    "audio_flac", subset=val_subset, sampling_rate=48000, fmt=".flac"
+)
+test_dataset = TheSessionDataset(
+    "audio_flac", subset=test_subset, sampling_rate=48000, fmt=".flac"
+)
+
+print("Training data", len(train_dataset))
+print("Validation data", len(val_dataset))
+print("Test data", len(test_dataset))
+
+# Model
+model = TheSessionModel()
+model.load_ckpt()
+
+model.toggle_gradients(False)
+model.toggle_gradients(True, ["clap_model.model.audio_projection"])
+
+# Training
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+
+train_model(
+    model,
+    train_dataset,
+    val_dataset,
+    optimizer,
+    epochs=10,
+    device="cuda",
+    batch_size=32,
+    num_workers=4,
+    destination="training/transfer_learning_audio_projection.csv",
+    best_model_path="models/transfer_learning_audio_projection.pt",
+)
