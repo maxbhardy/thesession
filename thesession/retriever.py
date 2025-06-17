5dffbbd862dc497c6f39ae7b0e3dc35250eaf0e3
Maxime Hardy <maxime.bonneau.hardy@gmail.com>
2025-06-17 06:09:06 -0400

    Retriever: durée fixe du signal en option

--- thesession/retriever.py
+++ thesession/retriever.py
@@ -1,13 +1,21 @@
 import pathlib
 import psycopg
 
+import numpy as np
 import torch
 import torchaudio
 import pandas as pd
 
 from .model import TheSessionModel
 
-def get_database_url(user: str, password: str, host: str = "localhost", port: str | int = 5432, database: str | None = None):
+
+def get_database_url(
+    user: str,
+    password: str,
+    host: str = "localhost",
+    port: str | int = 5432,
+    database: str | None = None,
+):
     if database is None:
         database = user
 
@@ -22,12 +30,19 @@ class TheSessionRetriever:
     device: str | None
     query = str
 
-    def __init__(self, database_url: str, model_weights: str | pathlib.Path, sampling_rate: int = 48000, backend: str | None = None, device: str | None = None):
+    def __init__(
+        self,
+        database_url: str,
+        model_weights: str | pathlib.Path,
+        sampling_rate: int = 48000,
+        backend: str | None = None,
+        device: str | None = None,
+    ):
         self.database_url = database_url
         self.sampling_rate = sampling_rate
         self.backend = backend
         self.device = device
-        
+
         self.model = TheSessionModel()
         self.model.toggle_gradients(False, verbose=False)
         self.model.load(model_weights)
@@ -51,37 +66,47 @@ class TheSessionRetriever:
         LIMIT %s
         """
 
-    def load_audio(self, filepath: str | pathlib.Path) -> torch.Tensor:
-        waveform, sr = torchaudio.load(filepath, backend=self.backend)
+    def load_audio(
+        self, filepath: str | pathlib.Path, duration: int | None = None
+    ) -> torch.Tensor:
+        signal, sr = torchaudio.load(filepath, backend=self.backend)
 
         if self.device:
-            waveform = waveform.to(self.device)
+            signal = signal.to(self.device)
 
         if sr != self.sampling_rate:
-            waveform = torchaudio.functional.resample(waveform, sr, self.sampling_rate)
+            signal = torchaudio.functional.resample(signal, sr, self.sampling_rate)
 
-        return waveform.mean(dim=0)
-    
-    def compute_embedding(self, data: torch.Tensor, unsqueeze: bool = False) -> torch.Tensor:
+        if duration:
+            length = len(signal)
+            expected_length = int(np.round(duration * self.sampling_rate))
+            repeats = int(np.ceil(expected_length / length))
+            signal = torch.tile(signal, repeats)[:expected_length]
+
+        return signal.mean(dim=0)
+
+    def compute_embedding(
+        self, data: torch.Tensor, unsqueeze: bool = False
+    ) -> torch.Tensor:
         if unsqueeze:
             return self.model(data.unsqueeze(0)).flatten()
         else:
             return self.model(data)
-        
 
-    def __call__(self, filepath: str | pathlib.Path, limit: int = 5) -> pd.DataFrame:
+    def __call__(
+        self, filepath: str | pathlib.Path, limit: int = 5, duration: int | None = None
+    ) -> pd.DataFrame:
         # Load audio into tensor
         filepath = pathlib.Path(filepath)
-        waveform = self.load_audio(filepath)
+        signal = self.load_audio(filepath, duration=duration)
 
         # Compute embedding of audio file
-        embedding = self.compute_embedding(waveform, unsqueeze=True)
+        embedding = self.compute_embedding(signal, unsqueeze=True)
 
         # Get best matches from database
         with psycopg.connect(self.database_url) as con:
             cursor = con.execute(self.query, (embedding.cpu().tolist(), limit))
             rows = cursor.fetchall()
-
 
         df = pd.DataFrame(
             rows,
@@ -94,9 +119,8 @@ class TheSessionRetriever:
                 "TuneType",
                 "Tunebooks",
                 "TuneVersion",
-                "Similarity"
-            ]
+                "Similarity",
+            ],
         )
 
         return df
-
