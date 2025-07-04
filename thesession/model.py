24f3e6dd9edb1e7df030bb3ff6830affe56556f4
Maxime Hardy <maxime.bonneau.hardy@gmail.com>
2025-07-04 10:30:16 -0400

    CLAP Audio model
    Enlever le modèle textuel permet de réduire la taille des poids de 635 Mo
    à 130 Mo, ce qui est considérable. On pourrait même envisager une exécution
    sur mobile avec cette taille de modèle, bien que loader le modèle resterait
    long.

--- thesession/model.py
+++ thesession/model.py
@@ -1,6 +1,10 @@
 import pathlib
 import torch
+import librosa
+
 from laion_clap import CLAP_Module
+from laion_clap.clap_module.htsat import create_htsat_model
+from laion_clap.training.data import get_audio_features
 
 
 class TheSessionModel(torch.nn.Module):
@@ -58,3 +62,128 @@ class TheSessionModel(torch.nn.Module):
                     )
 
                 param.requires_grad = requires_grad
+
+
+
+class AudioConfig:
+    model_type: str = "HTSAT"
+    model_name: str = "tiny"
+    sample_rate: int = 48000
+    # Param
+    audio_length: int = 1024
+    window_size: int = 1024
+    hop_size: int = 480
+    fmin: int = 50
+    fmax: int = 14000
+    class_num: int = 527
+    mel_bins: int = 64
+    clip_samples: int = 480000
+
+    def asdict(self):
+        return {k: getattr(self, k) for k in self.__annotations__}
+
+
+class CLAPAudioModel(torch.nn.Module):
+    def __init__(self, enable_fusion: bool = True, device: str | None = None):
+        super().__init__()
+
+        self.audio_cfg = AudioConfig()
+        self.enable_fusion = enable_fusion
+        self.device = device
+
+        # Model audio branch
+        self.audio_branch = create_htsat_model(self.audio_cfg, self.enable_fusion, "aff_2d")
+
+        # Model audio projection
+        self.audio_projection = torch.nn.Sequential(
+            torch.nn.Linear(768, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 512)
+        )
+
+        # Send model to device
+        if self.device:
+            self.audio_branch = self.audio_branch.to(self.device)
+            self.audio_projection = self.audio_projection.to(self.device)
+
+    def get_audio_features(self, audio_waveform: torch.Tensor) -> dict[str, torch.Tensor]:
+        audio_features = get_audio_features(
+            {},
+            audio_waveform,
+            480000,
+            data_truncating='fusion' if self.enable_fusion else 'rand_trunc', 
+            data_filling='repeatpad',
+            audio_cfg=self.audio_cfg.asdict(),
+            require_grad=audio_waveform.requires_grad
+        )
+
+        return audio_features
+
+    def get_audio_embedding_from_filelist(self, x: list[str | pathlib.Path]) -> torch.Tensor:
+        """get audio embeddings from the audio file list
+
+        Parameters
+        ----------
+        x: List[str] (N,): 
+            an audio file list to extract features, audio files can have different
+            lengths (as we have the feature fusion machanism)
+
+        Returns
+        ----------
+        audio_embed : torch.Tensor (N,D):
+            audio embeddings that extracted from audio files
+        """ 
+        audio_input = []
+
+        for f in x:
+            # load the waveform of the shape (T,), should resample to 48000
+            audio_waveform, _ = librosa.load(f, sr=48000)           
+            audio_waveform = torch.from_numpy(audio_waveform).float()
+
+            tmp_dict = self.get_audio_features(audio_waveform)
+
+            audio_input.append(tmp_dict)
+
+        audio_embed = self.get_audio_embedding(audio_input)
+
+        return audio_embed
+
+    def get_audio_embedding_from_data(self, x: torch.Tensor) -> torch.Tensor:
+        """get audio embeddings from the audio data
+
+        Parameters
+        ----------
+        x: torch.Tensor (N,T): 
+            audio data, must be mono audio tracks.
+        
+        Returns
+        ----------
+        audio embed: torch.Tensor (N,D):
+            audio embeddings that extracted from audio files
+        """ 
+        audio_input = []
+
+        for audio_waveform in x:          
+            tmp_dict = self.get_audio_features(audio_waveform)
+            audio_input.append(tmp_dict)
+
+        audio_embed = self.get_audio_embedding(audio_input)
+
+        return audio_embed
+
+    def get_audio_embedding(self, data: list[dict[str, torch.Tensor]]) -> torch.Tensor:
+        input_dict = {}
+        keys = data[0].keys()
+
+        for k in keys:
+            input_dict[k] = torch.cat([d[k].unsqueeze(0) for d in data], dim=0).to(self.device)
+
+        return self.forward(input_dict)
+
+    def forward(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
+        x = self.audio_branch(x, mixup_lambda=None, device=self.device)["embedding"]
+        x = self.audio_projection(x)
+        x = torch.nn.functional.normalize(x, dim=-1)
+
+        return x
+
